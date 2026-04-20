662d372a7786f6bb91e46c009d3c0529bf7cabc9
Florencia Baeza <florencia.baeza@uc.cl>
2026-04-19 22:29:23 -0400

    Inicialización de las colas

--- src/DCCyclone/main.c
+++ src/DCCyclone/main.c
@@ -8,8 +8,7 @@ typedef enum {
     RUNNING,
     WAITING,
     FINISHED,
-    DEAD,
-    NOT_ARRIVED
+    DEAD
 } State;
 
 typedef struct {
@@ -33,14 +32,14 @@ typedef struct {
 
 typedef struct {
     Process **procesos; 
-    int tamaño;            
+    int size;            
     int capacidad;        
 } Queue;
 
 Queue* crear_queue(int capacidad) {
     Queue *q = (Queue*)malloc(sizeof(Queue));
     q->procesos = (Process**)malloc(sizeof(Process*) * capacidad);
-    q->tamaño = 0;
+    q->size = 0;
     q->capacidad = capacidad;
     return q;
 }
@@ -69,6 +68,7 @@ int main(int argc, char *argv[]) {
     fscanf(file, "%d", &agingThreshold);
     fscanf(file, "%d", &nProcesses);
 
+    Process **todos_los_procesos = (Process**)malloc(sizeof(Process*) * nProcesses);
     char name[20];
     int pid, start, burstTime, burstQty, ioTime, deadline;
 
@@ -76,7 +76,32 @@ int main(int argc, char *argv[]) {
         fscanf(file, "%s %d %d %d %d %d %d",
                name, &pid, &start, &burstTime,
                &burstQty, &ioTime, &deadline);
+        
+        Process *p = (Process*)malloc(sizeof(Process));
+        strcpy(p->name, name);
+        p->pid = pid;
+        p->t_inicio = start;
+        p->t_cpu_burst = burstTime;
+        p->n_bursts = burstQty;
+        p->io_wait = ioTime;
+        p->t_deadline = deadline;
+        p->rafagas_completadas = 0;
+        p->progreso_rafaga_actual = 0;
+        p->progreso_io_actual = 0;
+        p->quantum_consumido = 0;
+        p->t_lcpu = 0;
+        p->first_time_cpu = -1;
+        p->waiting_time = 0;
+        p->interrupciones = 0;
+        todos_los_procesos[i] = p;
     }
+
+    fclose(file);
+    Queue *cola_high = crear_queue(nProcesses);
+    Queue *cola_low = crear_queue(nProcesses);
+    Process *cpu_running = NULL; 
+    int procesos_terminados = 0; 
+    int tick = 0;
 
     fclose(file);
 
